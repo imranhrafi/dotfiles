e1c1cd03efbf06ed0220a281d0705e56ce259124
imranhrafi <senbagiimran@gmail.com>
2024-07-04 23:29:30 +0600

    Make certain changes and fix isuue on install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,25 @@
 
 DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
+# Alacritty configuration
 rm -rf $HOME/.config/alacritty
-ln -s $DOTFILES/ala $HOME/.config/alacritty
+ln -sT $DOTFILES/alacritty $HOME/.config/alacritty
 
-rm -rf $HOME/.tmux.conf
-ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+# Tmux configuration
+rm -f $HOME/.tmux.conf
+ln -sT $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 
+# Ensure local bin directory exists
 mkdir -p $HOME/.local/bin
 
-rm -rf $HOME/.local/bin/t
-ln -s $DOTFILES/scripts/t $HOME/.local/bin/t
+# Script link
+rm -f $HOME/.local/bin/t
+ln -sT $DOTFILES/scripts/t $HOME/.local/bin/t
 
+# Neovim configuration
 rm -rf $HOME/.config/nvim
-ln -s $DOTFILES/nvim $HOME/.config/nvim
+ln -sT $DOTFILES/nvim $HOME/.config/nvim
 
+# Hypr configuration
 rm -rf $HOME/.config/hypr
-ln -s $DOTFILES/nvim $HOME/.config/hypr
+ln -sT $DOTFILES/hypr $HOME/.config/hypr
