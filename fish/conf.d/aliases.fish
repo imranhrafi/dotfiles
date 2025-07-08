ce205fc34dea239adefbff7cf69c87a1b603c180
imranhrafi <senbagiimran@gmail.com>
2025-07-08 21:56:22 +0600

    feat(fish): enhance git help menu and add 'lsa' alias for listing all files

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,4 +1,6 @@
 # ===== FILE AND DIRECTORY MANAGEMENT =====
+# 'lsa' should show all files, including those starting with '.'
+alias lsa="eza -al --color=always --group-directories-first --icons"
 alias ls="eza -al --color=always --group-directories-first --icons"
 alias la="eza -a --color=always --group-directories-first --icons"
 alias ll="eza -l --color=always --group-directories-first --icons"
