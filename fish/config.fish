664367d4850b6cb5c97057801e201ae70617b720
imranhrafi <senbagiimran@gmail.com>
2025-07-10 16:39:04 +0600

    fixed config

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,11 @@
 # Hide welcome message
 set fish_greeting
 # (All other configuration is now modularized in conf.d/*.fish)
+# pnpm
+set -gx PNPM_HOME "/home/rafi/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+set -x GOOGLE_API_KEY "AIzaSyDVf872l488-oFuYvfsuyTTR5XH8qj2ZYc"
+
